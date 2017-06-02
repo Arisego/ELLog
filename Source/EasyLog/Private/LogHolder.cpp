1569429c4be1090e297ac015951b6b77a3116bb2
Arisego <3301325+Arisego@users.noreply.github.com>
2017-06-02 18:06:41 +0800

    [C]Write to path from FPaths
    todo:
    - Rec create folder
    - Config read path
    - Rotate log file

--- Source/EasyLog/Private/LogHolder.cpp
+++ Source/EasyLog/Private/LogHolder.cpp
@@ -3,35 +3,73 @@
 #include "EasyLog.h"
 #include "LogHolder.h"
 
+#include "Paths.h"
+#include "PlatformFilemanager.h"
+#include "GenericPlatformFile.h"
+
+#include "AllowWindowsPlatformTypes.h"
+
 #include "easyloggingpp/easylogging++.h"
 
+#include "HideWindowsPlatformTypes.h"
+
+
+
+
+
 INITIALIZE_EASYLOGGINGPP
+
+namespace LogHolderNs
+{
+	el::base::SubsecondPrecision DefaultSubSecPre;
+};
 
 
 LogHolder::LogHolder()
 {
+	FString t_fsLogPath = FPaths::RootDir();
+	t_fsLogPath += "logs/";
+	//t_fsLogPath += "dlo/gs/";
+
+	//UE_LOG(LogTemp, Log, TEXT("Parent path: %s"), *FPaths::GetPath(FPaths::GetPath(t_fsLogPath)));
+
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (!PlatformFile.DirectoryExists(*t_fsLogPath))
+	{
+		FPlatformFileManager::Get().GetPlatformFile().CreateDirectory(*t_fsLogPath);
+
+		if (!PlatformFile.DirectoryExists(*t_fsLogPath))
+		{
+			t_fsLogPath = FPaths::RootDir();
+			UE_LOG(LogTemp, Warning, TEXT("[LogHolder] LogHolder(): Unable to create log directory"));
+		}
+	}
+
+	t_fsLogPath += el::base::utils::DateTime::getDateTime("%Y%M%d_%H_%m_%s", &LogHolderNs::DefaultSubSecPre).c_str();
+	t_fsLogPath += ".log";
+
 	el::Configurations defaultConf;
 	defaultConf.setToDefault();
 	// Values are always std::string
 	defaultConf.set(el::Level::Global,
 		el::ConfigurationType::Format, "%datetime %level %msg");
 	defaultConf.set(el::Level::Global,
-		el::ConfigurationType::Filename, "E:/log.log");
+		el::ConfigurationType::Filename, TCHAR_TO_ANSI(*t_fsLogPath));
 
 	// default logger uses default configurations
 	el::Loggers::reconfigureLogger("default", defaultConf);
 
-	LOG(INFO) << "[FEasyLogModule] StartupModule()";
-	LOG(INFO) << "[FEasyLogModule] StartupModule() --- Begin";
-	UE_LOG(LogTemp, Log, TEXT("[FEasyLogModule] StartupModule() - ue_log"));
+	LOG(INFO) << "[LogHolder] LogHolder(): Construct";
+	UE_LOG(LogTemp, Log, TEXT("[LogHolder] LogHolder(): Construct with path: %s"), *t_fsLogPath);
 }
 
 LogHolder::~LogHolder()
 {
+	LOG(INFO) << "[LogHolder] LogHolder(): Destruct";
 }
 
 void LogHolder::LogToFile(FString InString)
 {
 	UE_LOG(LogTemp, Log, TEXT("[LogHolder] LogToFile()"));
-	LOG(INFO) << "[LogHolder] LogToFile()" << *InString;
+	LOG(INFO) << "[LogHolder] LogToFile(): " << *InString;
 }
